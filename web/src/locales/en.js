98265cf6a4ffa53b7228260b803f596074145926
CodeingBoy <CodeingBoy@gmail.com>
2019-11-24 20:39:31 +0800

    Add language resources

--- web/src/locales/en.js
+++ web/src/locales/en.js
@@ -4,6 +4,9 @@ export default {
 	fileSize: 'File Size',
 	mainDrive: 'Main Drive',
 	search: 'Search',
+	save: 'Save',
+	close: 'Close',
+	start: 'Start',
 	fileUpload: 'File Upload',
 	urlUpload: 'Upload from url',
 	upload: 'Upload',
@@ -11,5 +14,26 @@ export default {
 	uploading: 'Uploading...',
 	serverProcessing: 'Server is processing the file now',
 	bigFileUploadWarning:
-		"Due to CloudFlare Workers' limitation, uploading bigfiles may randomly failed."
+		"Due to CloudFlare Workers' limitation, uploading bigfiles may randomly failed.",
+	aria2Download: 'Download with Aria2',
+	aria2DownloadRecursive: 'Recursive download subfolders',
+	aria2FetchingFiles:
+		'Fetching files, remaining {remainingFolderCount} folders',
+	aria2DownloadStats:
+		'Will download {fileCount} files(in {folderCount} folders), {totalDownloadSize} in total',
+	aria2DownloadProgress:
+		'Adding download tasks, remaining {remainingFileCount} files',
+	aria2DownloadSuccess: 'All files added',
+	aria2DownloadFailed: 'Fail adding download tasks, reason: {failReason}',
+	aria2FetchFileFailed: 'Fail fetching files, reason: {failReason}',
+	aria2RPCSettings: 'Aria2 RPC Settings',
+	aria2RPCHost: 'Host',
+	aria2RPCPort: 'Port',
+	aria2RPCPath: 'Path',
+	aria2RPCToken: 'Token',
+	aria2RPCDownloadPath: 'Download Path',
+	aria2TestConnection: 'Test Connection',
+	aria2Testing: 'Testing',
+	aria2TestFailed: 'Connect failed, reason: {reason}',
+	aria2TestSucceed: 'Connect succeed, version: {version}'
 }
--- web/src/locales/zh-CN.js
+++ web/src/locales/zh-CN.js
@@ -6,6 +6,9 @@ export default {
 	fileSize: '文件大小',
 	mainDrive: '主要硬盘',
 	search: '搜寻',
+	save: '保存',
+	close: '关闭',
+	start: '开始',
 	fileUpload: '文件上传',
 	urlUpload: '从网址上传',
 	upload: '上传',
@@ -14,5 +17,25 @@ export default {
 	serverProcessing: '服务器正在处里文件',
 	bigFileUploadWarning:
 		'由于 CloudFlare Workers 的限制，上传大档案可能会随机失败',
-	$vuetify
+	$vuetify,
+	aria2Download: '使用 Aria2 下载',
+	aria2DownloadRecursive: '包含子文件夹',
+	aria2FetchingFiles:
+		'正在获取文件，还剩 {remainingFolderCount} 个文件夹待获取',
+	aria2DownloadStats:
+		'将会下载 {folderCount} 个文件夹中的 {fileCount} 个文件，总计大小 {totalDownloadSize}',
+	aria2DownloadProgress: '正在添加下载任务，还剩 {remainingFileCount} 个文件',
+	aria2DownloadSuccess: '已添加所有文件下载',
+	aria2DownloadFailed: '添加下载任务失败，原因：{failReason}',
+	aria2FetchFileFailed: '获取文件列表失败，原因：{failReason}',
+	aria2RPCSettings: 'Aria2 RPC 配置',
+	aria2RPCHost: '主机',
+	aria2RPCPort: '端口',
+	aria2RPCPath: '路径',
+	aria2RPCToken: '密码',
+	aria2RPCDownloadPath: '下载路径',
+	aria2TestConnection: '测试连接',
+	aria2Testing: '正在尝试连接',
+	aria2TestFailed: '连接失败，原因：{reason}',
+	aria2TestSucceed: '连接成功，Aria2 版本：{version}'
 }
