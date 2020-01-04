96b4a2487ac40ae9adef3f960a05cd87ed6347a1
DarkSky <darksky2048@gmail.com>
2020-01-04 22:11:35 +0800

    subdir support for web

--- web/src/api.js
+++ web/src/api.js
@@ -1,9 +1,24 @@
 import xf from './xfetch'
 
+const subdir = window.props.subdir
+
+export function getSubDirPath(path) {
+	if (typeof path === 'string') {
+		if (path.startsWith('/')) {
+			return `/${subdir}${path}`
+		} else {
+			return `/${subdir}/${path}`
+		}
+	}
+	return path
+}
+
 const headers = {}
+
 if (localStorage.token) {
 	headers.Authorization = 'Basic ' + localStorage.token
 }
+
 export default xf.extend({
 	baseURI: window.props.api,
 	headers
